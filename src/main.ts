f3a21013687ce0955ec4e8bf6d7a93359dc604e8
chensongbai <1210828392@qq.com>
2021-07-23 10:21:50 +0800

    chore: 添加自适应的包

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-07-21 11:51:46
- * @LastEditTime: 2021-07-22 14:42:35
+ * @LastEditTime: 2021-07-23 10:19:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /hello-vue3/src/main.ts
  */
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'amfe-flexible';
 // 通用字体
 import 'vfonts/Lato.css'
 createApp(App).mount('#app')
